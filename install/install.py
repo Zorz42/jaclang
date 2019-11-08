973c5f720e9b71268cfd7597eb0609fee3017284
Zorz42 <jakob@zorz.si>
2019-11-08 22:13:06 +0100

    fixed install bug

--- install/install.py
+++ install/install.py
@@ -44,31 +44,7 @@ elif len(sys.argv) == 2:
 		
 		if decision("Would you like to clean up object files?"):
 			system("make clean")
-		
-		print("Creating alias...")
-		alias_file_name = ""
-		if platform.system() == 'Linux':
-			alias_file_name = ".bashrc"
-		elif platform.system() == 'Darwin':
-			alias_file_name = ".bash_profile"
-		else:
-			print("unsuported os")
-			exit(1)
-
-		alias_command = "complete -o default -W \"uninstall version versionid versionstr\" 'jaclang'\n"
-		
-		system("sudo touch " + path.expanduser("~") + "/" + alias_file_name)
-		
-		alias_file = open(path.expanduser("~") + "/" + alias_file_name, "r")
-		lines = alias_file.readlines()
-		if len(lines) != 0:
-			lines = lines[-1]
-		alias_file.close()
-
-		if lines != alias_command:
-			alias_file = open(path.expanduser("~") + "/" + alias_file_name, "a")
-			alias_file.write(alias_command)
-			alias_file.close()
+		system("sudo python3 install/makealias.py")
 		print("Jaclang installed sucsessfully! Type jaclang in terminal for help.")
 	
 	elif sys.argv[1] == "dependencies":
---
+++ install/makealias.py
@@ -0,0 +1,30 @@
+from os import system, path
+import platform
+
+print("Creating alias ... ", end='')
+alias_file_name = ""
+if platform.system() == 'Linux':
+	alias_file_name = ".bashrc"
+elif platform.system() == 'Darwin':
+	alias_file_name = ".bash_profile"
+else:
+	print("unsuported os")
+	exit(1)
+
+alias_command = "complete -o default -W \"uninstall version versionid versionstr\" 'jaclang'\n"
+		
+system("sudo touch " + path.expanduser("~") + "/" + alias_file_name)
+		
+alias_file = open(path.expanduser("~") + "/" + alias_file_name, "r")
+lines = alias_file.readlines()
+if len(lines) != 0:
+	lines = lines[-1]
+alias_file.close()
+
+if lines != alias_command:
+	alias_file = open(path.expanduser("~") + "/" + alias_file_name, "a")
+	alias_file.write(alias_command)
+	alias_file.close()
+	print("DONE")
+else:
+	print("ALREADY MADE")
