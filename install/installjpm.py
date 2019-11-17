d2832ca67b2d63839052258dd2bb6b4a56a62bde
Zorz42 <jakob@zorz.si>
2019-11-17 14:39:49 +0100

    jpm now checks for dependencies

--- install/installjpm.py
+++ install/installjpm.py
@@ -2,5 +2,5 @@ import wget, os, ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 wget.download("https://github.com/Zorz42/jpm/archive/master.zip")
 os.system("unzip jpm-master.zip")
-os.system("cd jpm-master && python3 install.py install")
+os.system("cd jpm-master && python3 install.py dependencies && python3 install.py install")
 os.system("rm jpm-master.zip; rm -r jpm-master")
