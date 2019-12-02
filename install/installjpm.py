df27f07617a50dd6cbc1f0b0fac9708ec5c91a78
Zorz42 <jakob@zorz.si>
2019-12-02 10:59:47 +0100

    improved install process even more

--- install/installjpm.py
+++ install/installjpm.py
@@ -7,8 +7,13 @@ except:
 def no_bar(a, b, c):
 	pass
 
-print("Downloading jpm")
+print()
+print("Downloading jpm ... ", end='', flush=True)
 wget.download("https://github.com/Zorz42/jpm/archive/master.zip", bar=no_bar)
+print("DONE")
+
+print("Installing jpm ... ", end='', flush=True)
 os.system("unzip -q jpm-master.zip")
-os.system("cd jpm-master && python3 install.py install")
+os.system("cd jpm-master && python3 install.py quietinstall")
 os.system("rm jpm-master.zip; rm -r jpm-master")
+print("DONE")
