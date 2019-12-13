33462875054c546cf0b8a8bed674f52bd37fb022
Zorz42 <jakob@zorz.si>
2019-12-13 22:10:36 +0100

    improved build process

--- install/build.py
+++ install/build.py
@@ -8,7 +8,17 @@ import sys
 from settings import *
 
 compiled_count = 0
-new_header = False
+
+platform = None
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    platform = "linux"
+elif sys.platform == "darwin":
+    platform = "OSX"
+else:
+    print("Unsupported platform!")
+    exit(1)
+
 
 class BuildThread(threading.Thread):
     def __init__(self, count, filename):
@@ -19,16 +29,21 @@ class BuildThread(threading.Thread):
 
     def run(self):
         global compiled_count
-        current_thread = Popen(
-            ("g++ -w -pipe -m64 -std=gnu++11 -I" + includedir + " -o " + objdir + self.name + ".o -c " +
-             srcdir + self.name + ".cpp -include-pch build/jaclang.h.gch").split(" "))
+        compile_command = None
+        if platform == "linux":
+            compile_command = "g++ -w -pipe -m64 -std=gnu++11 -I" + includedir + " -I" + objdir + " -o " + objdir + \
+                              self.name + ".o -c " + srcdir + self.name + ".cpp"
+        elif platform == "OSX":
+            compile_command = "g++ -w -pipe -m64 -std=gnu++11 -I" + includedir + " -o " + objdir + self.name + \
+                              ".o -c " + srcdir + self.name + ".cpp -include-pch build/jaclang.h.gch"
+        current_thread = Popen(compile_command.split(" "))
         while current_thread.poll() is None:
             pass
         columns = int(popen('stty size', 'r').read().split()[1]) - 2
         for i in range(columns + 2):
             print(" ", end='')
         print("\r", end='')
-        print("[CC] [-FLAGS] " + srcdir +  self.name + ".cpp -> " + objdir +  self.name + ".o")
+        print("[CC] [-FLAGS] " + srcdir + self.name + ".cpp -> " + objdir + self.name + ".o")
         compiled_count += 1
         print_progress_bar(compiled_count, self.objlen, columns)
 
@@ -51,11 +66,11 @@ def build(fail=False):
     if not path.isdir(objdir):
         system("mkdir " + objdir)
     files = [file.split('.')[0] for file in listdir(srcdir) if len(file.split('.')) == 2]
-    srcfilesdirs = [dir for dir in listdir(srcdir) if len(dir.split('.')) == 1]
-    for dir in srcfilesdirs:
-        if not path.isdir("build/" + dir):
-            system("mkdir " + objdir + dir)
-        files += [dir + "/" + file.split('.')[0] for file in listdir(srcdir + dir) if len(file.split('.')) == 2]
+    srcfilesdirs = [Dir for Dir in listdir(srcdir) if len(Dir.split('.')) == 1]
+    for Dir in srcfilesdirs:
+        if not path.isdir("build/" + Dir):
+            system("mkdir " + objdir + Dir)
+        files += [Dir + "/" + str(file.split('.')[0]) for file in listdir(srcdir + Dir) if len(file.split('.')) == 2]
     count = 0
     threads = []
     for file in files:
@@ -63,14 +78,19 @@ def build(fail=False):
         if not path.isfile(objdir + file + ".o") or path.getctime(objdir + file + ".o") < path.getctime(
                 srcdir + file + ".cpp"):
             threads.append(BuildThread(count, file))
-    if threads:
-        for file in listdir(includedir):
-            if not path.isfile(objdir + "jaclang.h.gch") or path.getctime(objdir + "jaclang.h.gch") < path.getctime(includedir + file):
-                print("Building jaclang header...")
+    for file in listdir(includedir):
+        if not path.isfile(objdir + "jaclang.h.gch") or path.getctime(objdir + "jaclang.h.gch") < \
+                path.getctime(includedir + file):
+            print("Building jaclang header...")
+            if platform == "linux":
+                system("g++ -c " + includedir + "jaclang.h -o " + objdir + "jaclang.h.gch -w")
+            elif platform == "OSX":
                 system("g++ " + includedir + "jaclang.h -o " + objdir + "jaclang.h.gch -std=gnu++11 -stdlib=libc++ -w")
-                new_header = True
-                break
-        print("Building jaclang...")
+            threads = []
+            for file2 in files:
+                threads.append(BuildThread(count, file2))
+            break
+    print("Building jaclang...")
     for thread in threads:
         thread.objlen = len(threads)
         thread.start()
