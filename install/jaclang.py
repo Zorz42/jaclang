89b5e6790cdbe4e94e95f34f1da4ef4b22b82016
Zorz42 <jakob@zorz.si>
2020-01-16 13:43:26 +0100

    adding better completion

---
+++ install/jaclang.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+curr_word = int(argv[1])
+args = argv[3:]
+
+def main():
+    if curr_word == 1:
+        if len(args) == 0:
+        	return ["version", "versionid", "versionstr"]
+        else:
+    else:
+        return ["aaa"]
+
+if __name__ == "__main__":
+    print(" ".join(main()))
