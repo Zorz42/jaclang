a0e078e81aeac983984aa851cb8384cee1089109
Zorz42 <jakob@zorz.si>
2020-01-19 21:29:03 +0100

    made most of the autocompletion for the jpm

--- jaclang-data/jpm_completion.py
+++ jaclang-data/jpm_completion.py
@@ -1,4 +1,5 @@
 from sys import argv
+from os import popen
 
 curr_word = int(argv[1])
 args = argv[3:]
@@ -6,9 +7,23 @@ args = argv[3:]
 
 def main():
     if curr_word == 1:
-        return ["version", "versionid", "versionstr"]
+        return ["install", "remove", "list", "cleanup", "upgrade", "repair", "listall", "version"]
     elif curr_word == 2:
-        return []
+        if args[0] == "install":
+            packages = popen("jpm listall").read().split("\n")[4].split(" ")
+            packages = [package for package in packages if package != ""]
+            for i in range(len(packages)):
+                while packages[i][0] != 'm':
+                    packages[i] = packages[i][1:]
+                packages[i] = packages[i][1:]
+            packages = [package for package in packages if package != ""]
+            return packages
+        elif args[0] == "remove":
+            return [package[4:] for package in popen("jpm list").read().split("\n")[2:]]
+        elif args[0] == "upgrade":
+            return []
+        else:
+            return []
     else:
         return []
 
