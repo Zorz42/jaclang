8c76dc9a6ff9df30e36c84c0eb0cf792b1cacdde
Zorz42 <jakob@zorz.si>
2020-01-20 14:19:25 +0100

    improved som bash completion

--- data/jpm.bash
+++ data/jpm.bash
@@ -6,9 +6,16 @@ _jpm()
     # shellcheck disable=SC2034
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="$(python3 /usr/local/bin/jaclang-data/jpm_completion.py $COMP_CWORD ${COMP_WORDS[*]})"
-
     # shellcheck disable=SC2207
     COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+
+    if ["${COMP_WORDS[1]}" == "install"]; then
+    
+	test
+        #echo adgsgag
+
+    fi
+
     return 0
 }
 complete -F _jpm jpm
