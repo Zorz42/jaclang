4ff5c2a49cddf97d715174e27f590e3462f89e2c
Zorz42 <jakob@zorz.si>
2020-03-14 09:39:57 +0100

    Update installjpm.py

--- install/installjpm.py
+++ install/installjpm.py
@@ -1,3 +1,4 @@
+
 import ssl
 from getpass import getuser
 from os import getcwd, path, mkdir, system
@@ -18,29 +19,30 @@ def installjpm_main():
         mkdir(install_folder)
     print()
     print("Downloading jpm ... ", end='', flush=True)
-    download("https://github.com/Zorz42/jpm/archive/master.zip", bar=None)
+    download("https://github.com/Zorz42/jpm/archive/stable.zip", bar=None)
     print("DONE")
 
     print("Installing jpm ... ", end='', flush=True)
     from zipfile import ZipFile
-    with ZipFile(getcwd() + "/jpm-master.zip", 'r') as zip_ref:
+    with ZipFile(getcwd() + "/jpm-stable.zip", 'r') as zip_ref:
         zip_ref.extractall(getcwd())
 
     dirs = ['to_install', 'librarysources', 'metadatas']
     for Dir in dirs:
-        if not path.isdir("jpm-master/jpm-sources/" + Dir):
-            mkdir('jpm-master/jpm-sources/' + Dir)
+        if not path.isdir("jpm-stable/jpm-sources/" + Dir):
+            mkdir('jpm-stable/jpm-sources/' + Dir)
     if sys() == 'Linux':
-        system("sudo cp -r jpm-master/jpm-sources " + install_folder)
+        system("sudo cp -r jpm-stable/jpm-sources " + install_folder)
     elif sys() == 'Darwin':
-        system("sudo cp -r jpm-master/jpm-sources " + install_folder + "jpm-sources")
+        system("sudo cp -r jpm-stable/jpm-sources " + install_folder + "jpm-sources")
     else:
         print("Unsupported os!")
-    system("sudo cp jpm-master/jpm /usr/local/bin")
+    system("sudo rm /usr/local/bin/jpm")
+    system("sudo cp jpm-stable/jpm /usr/local/bin")
     system("sudo chown " + getuser() + " " + install_folder + "jpm-sources")
     system("sudo chmod +x /usr/local/bin/jpm")
 
-    system("rm jpm-master.zip; rm -r jpm-master")
+    system("rm jpm-stable.zip; rm -r jpm-stable")
     print("DONE")
     print("Initializing jpm...")
     print()
