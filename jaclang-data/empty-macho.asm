c39530f5d5db81c9b26e586b1d2d92ecf922d47b
Zorz42 <jakob@zorz.si>
2020-03-18 17:41:12 +0100

    Update empty-macho.asm

--- jaclang-data/empty-macho.asm
+++ jaclang-data/empty-macho.asm
@@ -1,6 +1,7 @@
 global start
 section .data
 
+returnvalue: dq 0
 stdoutchar: db 0
 section .bss
 
@@ -21,3 +22,35 @@ printchar:
    syscall
 
    ret
+
+%macro pusha 0
+   push rbx
+   push rcx
+   push rsi
+   push rdi
+   push r8
+   push r9
+   push r10
+   push r11
+   push r12
+   push r13
+   push r14
+   push r15
+   push rbp
+%endmacro
+
+%macro popa 0
+   pop rbp
+   pop r15
+   pop r14
+   pop r13
+   pop r12
+   pop r11
+   pop r10
+   pop r9
+   pop r8
+   pop rdi
+   pop rsi
+   pop rcx
+   pop rbx
+%endmacro
