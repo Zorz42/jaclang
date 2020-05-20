e1c20e07ef82459e30e5798f99535891a5379a45
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-05-20 17:06:54 +0200

    Update flowControl.cpp

--- src/generator/flowControl.cpp
+++ src/generator/flowControl.cpp
@@ -4,16 +4,18 @@
 
 #define current currentBranchScope->sub->at(currentBranchScope->count)
 
+unsigned int ifCounter = 0;
+
 void generator::e::ifStatement() {
     if(current.sub->at(0).name == "calc") {
         generator::e::calculation(current.sub->at(0));
         file::append_instruction("cmpq", "$0", "%rbx");
-        file::append_instruction("je", "ifend");
+        file::append_instruction("je", "IFE" + std::to_string(ifCounter));
         currentBranchScope->count++;
-        
         scope();
         
-        file::append_instruction("ifend:");
+        file::append_instruction("IFE" + std::to_string(ifCounter) + ":");
+        ifCounter++;
     }
     else {
         if(current.sub->at(0).name != "0") {
